2a63a2253d500a339d2dd3011418611708dcfe7b
anastasia.bondareva <anastasia.bondareva@williamhill.co.uk>
2023-03-07 18:45:11 +0000

    refactoring to common crud interface

--- petclinic-data/src/main/java/com/example/petclinic/services/VetService.java
+++ petclinic-data/src/main/java/com/example/petclinic/services/VetService.java
@@ -5,11 +5,5 @@ import com.example.petclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet Vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
